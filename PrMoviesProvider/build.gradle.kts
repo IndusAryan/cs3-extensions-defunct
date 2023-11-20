6ca1601cfcd85a660825a6463fa93fecb1eaf5db
IndusAryan <serena01011975@gmail.com>
2023-11-20 19:18:51 +0530

    rebuild stuff

--- PrMoviesProvider/build.gradle.kts
+++ PrMoviesProvider/build.gradle.kts
@@ -3,9 +3,10 @@ version = 1
 
 cloudstream {
     language = "hi"
+    // All of these properties are optional, you can safely remove them
 
     // description = "Lorem Ipsum"
-    authors = listOf("AryanInvader")
+     authors = listOf("darkdemon")
 
     /**
      * Status int as the following:
@@ -14,13 +15,11 @@ cloudstream {
      * 2: Slow
      * 3: Beta only
      * */
-
     status = 1 // will be 3 if unspecified
     tvTypes = listOf(
         "TvSeries",
         "Movie",
-        "Anime",
     )
 
-    iconUrl = "https://www.google.com/s2/favicons?domain=prmovies.green&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=prmovies.bio&sz=%size%"
 }
