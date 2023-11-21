87eee0f792a85dbf1c82010eebc1dd7e3b1441ad
IndusAryan <serena01011975@gmail.com>
2023-11-21 19:04:22 +0530

    add yugen anime

---
+++ YugenAnime/build.gradle.kts
@@ -0,0 +1,28 @@
+// use an integer for version numbers
+version = 1
+
+cloudstream {
+    // All of these properties are optional, you can safely remove them
+
+    description = "Watch Subbed & Dubbed Anime and Anime Movies"
+    authors = listOf("Hexated, AryanInvader")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1
+
+    tvTypes = listOf(
+        "AnimeMovie",
+        "Anime",
+        "OVA",
+        )
+
+    language = "en"
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=yugen.to&sz=%size%"
+}
