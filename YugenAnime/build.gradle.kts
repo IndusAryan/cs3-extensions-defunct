a88699e9be0144310dcb77dbeca1ef4771d69a20
IndusAryan <125901294+IndusAryan@users.noreply.github.com>
2024-01-19 16:18:17 +0530

    disable yugen for good, bruh

--- YugenAnime/build.gradle.kts
+++ YugenAnime/build.gradle.kts
@@ -1,10 +1,10 @@
 // use an integer for version numbers
-version = 3
+version = 4
 
 cloudstream {
     // All of these properties are optional, you can safely remove them
 
-    description = "Watch Subbed & Dubbed Anime and Anime Movies"
+    description = "Install from Hexated"
     authors = listOf("Hexated, AryanInvader")
 
     /**
@@ -14,7 +14,7 @@ cloudstream {
      * 2: Slow
      * 3: Beta only
      * */
-    status = 1
+    status = 0
 
     tvTypes = listOf(
         "AnimeMovie",
